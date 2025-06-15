9f53904c97d3be2582b8b07b1f1ba41a314d9d52
Neo Jones Sehlabela <94590381+Gooner-Jones@users.noreply.github.com>
2025-06-15 19:07:54 +0200

    Update stored_pro_load_silver.sql

--- scripts/silver/stored_pro_load_silver.sql
+++ scripts/silver/stored_pro_load_silver.sql
@@ -8,9 +8,9 @@
    - Truncates Silver tables.
    - Inserts transformed and cleansed data from Bronze into Silver tables.
 		
-Parameters: This stored procedure does not accept any parameters or return any values.
+   Parameters: This stored procedure does not accept any parameters or return any values.
 
-Usage Example: EXEC Silver.load_silver;
+   Usage Example: EXEC Silver.load_silver;
 */
 -- ================================================================================================================
 CREATE OR ALTER PROCEDURE silver.load_silver AS
