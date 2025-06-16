4a73f77a5ef847c7229982752f40a1dede448aff
Neo Jones Sehlabela <94590381+Gooner-Jones@users.noreply.github.com>
2025-06-16 21:44:10 +0200

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -4,7 +4,7 @@
 /*
     This script creates tables in the 'silver' schema, dropping existing tables 
     if they already exist.
-	  Run this script to re-define the DDL structure of 'bronze' Tables
+    Run this script to re-define the DDL structure of 'bronze' Tables
 */
   
 -- Creating silver.crm_cust_info table
