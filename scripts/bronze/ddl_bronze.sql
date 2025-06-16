402f7a3258248f68903b3f312d573c5e71e6a161
Neo Jones Sehlabela <94590381+Gooner-Jones@users.noreply.github.com>
2025-06-16 21:35:29 +0200

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -22,7 +22,7 @@ GO
 
 -- Create bronze.crm_prd_info table
 IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
-    DROP TABLE bronze.crm_prd_info;
+DROP TABLE bronze.crm_prd_info;
 GO
 CREATE TABLE bronze.crm_prd_info (prd_id       INT,
 				  prd_key      NVARCHAR(50),
@@ -35,7 +35,7 @@ GO
 
 -- Create bronze.crm_sales_details table
 IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
-    DROP TABLE bronze.crm_sales_details;
+DROP TABLE bronze.crm_sales_details;
 GO
 CREATE TABLE bronze.crm_sales_details (sls_ord_num  NVARCHAR(50),
 				       sls_prd_key  NVARCHAR(50),
@@ -50,7 +50,7 @@ GO
 
 -- Create bronze.erp_loc_a101 table
 IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
-    DROP TABLE bronze.erp_loc_a101;
+DROP TABLE bronze.erp_loc_a101;
 GO
 CREATE TABLE bronze.erp_loc_a101(cid    NVARCHAR(50),
 				 cntry  NVARCHAR(50));
@@ -58,16 +58,16 @@ GO
 
 -- Create bronze.erp_cust_az12 tabel
 IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
-    DROP TABLE bronze.erp_cust_az12;
+DROP TABLE bronze.erp_cust_az12;
 GO
 CREATE TABLE bronze.erp_cust_az12 (cid    NVARCHAR(50),
-					  bdate  DATE,
-					  gen    NVARCHAR(50));
+				   bdate  DATE,
+				   gen    NVARCHAR(50));
 GO
 
 -- Create bronze.erp_px_cat_g1v2 table
 IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
-    DROP TABLE bronze.erp_px_cat_g1v2;
+DROP TABLE bronze.erp_px_cat_g1v2;
 GO
 CREATE TABLE bronze.erp_px_cat_g1v2 (id           NVARCHAR(50),
 				     cat          NVARCHAR(50),
